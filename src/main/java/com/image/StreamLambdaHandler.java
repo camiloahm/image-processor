5a0ea458aa678dab0552adb0c8b70c6548df2e36
Camilo  Hernandez <camilo.hernandezmarin@jumbo.com>
2019-04-01 19:44:34 +0200

    update libraries

--- src/main/java/com/image/StreamLambdaHandler.java
+++ src/main/java/com/image/StreamLambdaHandler.java
@@ -6,19 +6,19 @@ import com.amazonaws.serverless.proxy.internal.testutils.Timer;
 import com.amazonaws.serverless.proxy.model.AwsProxyRequest;
 import com.amazonaws.serverless.proxy.model.AwsProxyResponse;
 import com.amazonaws.serverless.proxy.spring.SpringBootLambdaContainerHandler;
+
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestStreamHandler;
 import com.image.filter.CognitoIdentityFilter;
-import lombok.extern.slf4j.Slf4j;
 
 import javax.servlet.DispatcherType;
 import javax.servlet.FilterRegistration;
+
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.util.EnumSet;
 
-@Slf4j
 public class StreamLambdaHandler implements RequestStreamHandler {
     private static SpringBootLambdaContainerHandler<AwsProxyRequest, AwsProxyResponse> handler;
 
@@ -33,7 +33,7 @@ public class StreamLambdaHandler implements RequestStreamHandler {
             });
         } catch (ContainerInitializationException e) {
             // if we fail here. We re-throw the exception to force another cold start
-            log.error("StreamLambdaHandler error", e);
+            e.printStackTrace();
             throw new RuntimeException("Could not initialize Spring Boot application", e);
         }
     }
@@ -47,8 +47,5 @@ public class StreamLambdaHandler implements RequestStreamHandler {
     public void handleRequest(InputStream inputStream, OutputStream outputStream, Context context)
             throws IOException {
         handler.proxyStream(inputStream, outputStream, context);
-
-        // just in case it wasn't closed by the mapper
-        outputStream.close();
     }
 }
