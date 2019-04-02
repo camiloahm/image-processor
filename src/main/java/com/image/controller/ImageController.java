fb4190148bb0374b54fb59e48656a90c36b1d90e
Camilo  Hernandez <camilo.hernandezmarin@jumbo.com>
2019-04-03 00:27:44 +0200

    deployment steps

--- src/main/java/com/image/controller/ImageController.java
+++ src/main/java/com/image/controller/ImageController.java
@@ -48,7 +48,7 @@ public class ImageController extends RestResponseExceptionHandler {
             path = "/image/health",
             produces = {MediaType.APPLICATION_JSON_VALUE})
     public ResponseEntity<String> healthCheck() {
-        return ResponseEntity.ok("UP");
+        return ResponseEntity.ok("UP2");
     }
 
     @GetMapping(
